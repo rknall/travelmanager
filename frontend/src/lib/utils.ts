c1583b6a23b4c53a92d62dc8ed10a1a6724a615c
Roland Knall <rknall@gmail.com>
2025-11-29 15:16:43 +0100

    Fix gitignore to include frontend/src/lib

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Roland Knall <rknall@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-only
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
